114a753172c32ada9ad17e0486f8cf4de98c0754
adan-gilbert <adan.gilbert@3wa.io>
2024-02-02 16:23:13 +0000

    étape 5

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+/*
 //require
-require 'models/Category.php';
+require 'models/CategoryManager.php';
 
 //instancier
 $category = new Category("catégorie 1");
@@ -15,3 +16,19 @@ $category->setId(1);
 
 //var dump
 var_dump($category);
+*/
+
+
+require 'managers/CategoryManager.php';
+
+// Instanciation du CategoryManager avec les détails de connexion
+$categoryManager = new CategoryManager("localhost", "nom_de_la_base", "utilisateur", "mot_de_passe");
+
+// Affichage des informations du CategoryManager
+
+var_dump($categoryManager);
+
+
+
+var_dump($categories);
+
--- managers/CategoryManager.php
+++ managers/CategoryManager.php
@@ -1,1 +1,19 @@
 <?php
+
+require 'AbstractManager.php';
+
+class CategoryManager extends Manager {
+    // Constructeur
+    public function __construct($host, $dbName, $user, $password) {
+        parent::__construct($host, $dbName, $user, $password);
+    }
+
+    public function findAll() {
+        return []; 
+    }
+}
+
+$categoryManager = new CategoryManager("localhost", "nom_de_la_base", "utilisateur", "mot_de_passe");
+$categories = $categoryManager->findAll();
+
+
